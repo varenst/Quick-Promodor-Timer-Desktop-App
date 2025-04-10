415e39d0856079fcbfa42a41fc038b6cae9711e8
varenst <markulsvalentins@gmail.com>
2025-04-10 21:50:08 +0300

    Promodor App

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+// Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use std::error::Error;
+
+use slint::{Color, Timer};
+
+slint::include_modules!();
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let ui = AppWindow::new()?;
+
+    let ui_handle = ui.as_weak();
+
+    let mut is_timer_counting = false;
+
+    ui.on_start_timer(move || {
+        let ui = ui_handle.unwrap();
+        
+        is_timer_counting = !is_timer_counting;
+
+        ui.set_istimerrunning(is_timer_counting);
+    });
+
+    ui.on_get_formatedTime(move |seconds| {
+       let formattedseconds = seconds % 60;
+       let formattedminutes = (seconds / 60) % 60;
+
+       let final_string = format!("{:0>2}:{:0>2}", formattedminutes, formattedseconds);
+
+       return final_string.into();
+    });
+    
+    ui.run()?;
+
+    Ok(())
+}
